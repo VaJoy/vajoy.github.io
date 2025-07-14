960f49d1a370ea4db1443da3af9164c7575efc3a
VaJoy Lan <vajoy@qq.com>
2025-07-14 16:19:40 +0800

    new

--- shader-demo/index.55da5.js
+++ shader-demo/index.55da5.js
@@ -1,4 +1,4 @@
-System.register(["./application.cd531.js"], function (_export, _context) {
+System.register(["./application.6ef36.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
