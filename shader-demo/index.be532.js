e79d6e4b5907d5e5ec7a05e607aa7414aecaff7a
VaJoy Lan <vajoy@qq.com>
2025-07-14 17:23:10 +0800

    new

--- shader-demo/index.be532.js
+++ shader-demo/index.be532.js
@@ -1,4 +1,4 @@
-System.register(["./application.6ef36.js"], function (_export, _context) {
+System.register(["./application.4626e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
