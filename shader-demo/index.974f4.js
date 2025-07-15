a5b7fe954f044fc31061977b65f851e579ebded8
VaJoy Lan <vajoy@qq.com>
2025-07-15 12:51:45 +0800

    new

--- shader-demo/index.974f4.js
+++ shader-demo/index.974f4.js
@@ -1,4 +1,4 @@
-System.register(["./application.4626e.js"], function (_export, _context) {
+System.register(["./application.ace54.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
