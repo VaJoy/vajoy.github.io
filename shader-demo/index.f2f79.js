257419b7874600eb9970f0c6bb402418ce723e18
VaJoy Lan <vajoy@qq.com>
2025-08-04 18:33:13 +0800

    new

--- shader-demo/index.f2f79.js
+++ shader-demo/index.f2f79.js
@@ -1,4 +1,4 @@
-System.register(["./application.9a83a.js"], function (_export, _context) {
+System.register(["./application.295bd.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
