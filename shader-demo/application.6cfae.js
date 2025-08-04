4b680aa71b405b327886e2801b78a239de782dc9
VaJoy Lan <vajoy@qq.com>
2025-08-04 19:02:00 +0800

    new

--- shader-demo/application.6cfae.js
+++ shader-demo/application.6cfae.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.07282.json';
+          this.settingsPath = 'src/settings.e8f0f.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
