4385958c69b376634eafd7e626ec97f0384aa0b6
VaJoy Lan <vajoy@qq.com>
2025-08-04 18:14:38 +0800

    new

--- shader-demo/index.d0c2d.js
+++ shader-demo/index.d0c2d.js
@@ -1,4 +1,4 @@
-System.register(["./application.aaffa.js"], function (_export, _context) {
+System.register(["./application.9a83a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
