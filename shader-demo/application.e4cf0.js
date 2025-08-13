4d9fdb9e46015504e45cdd1c96c979c5d1662bc7
VaJoy Lan <vajoy@qq.com>
2025-08-14 02:26:42 +0800

    fix

--- shader-demo/application.e4cf0.js
+++ shader-demo/application.e4cf0.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.5d546.json';
+          this.settingsPath = 'src/settings.ce36c.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- shader-demo/index.6d1ef.js
+++ shader-demo/index.6d1ef.js
@@ -1,4 +1,4 @@
-System.register(["./application.c2311.js"], function (_export, _context) {
+System.register(["./application.e4cf0.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
