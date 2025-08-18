faffa7429f90d19d426ed5c0d096c221efb11915
VaJoy Lan <vajoy@qq.com>
2025-08-18 14:50:21 +0800

    new

--- shader-demo/index.737b0.js
+++ shader-demo/index.737b0.js
@@ -1,4 +1,4 @@
-System.register(["./application.e4cf0.js"], function (_export, _context) {
+System.register(["./application.77599.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
