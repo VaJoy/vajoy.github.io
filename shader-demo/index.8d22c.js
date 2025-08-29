240b6d7d061556ed773364f35a18cf9534ac1013
VaJoy Lan <vajoy@qq.com>
2025-08-29 20:56:55 +0800

    new

--- shader-demo/index.8d22c.js
+++ shader-demo/index.8d22c.js
@@ -1,4 +1,4 @@
-System.register(["./application.559fc.js"], function (_export, _context) {
+System.register(["./application.8a184.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
