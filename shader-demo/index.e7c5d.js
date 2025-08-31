ebf57bddf8f21272692423900f157a0426fc6924
VaJoy Lan <vajoy@qq.com>
2025-08-31 21:40:50 +0800

    new

--- shader-demo/index.e7c5d.js
+++ shader-demo/index.e7c5d.js
@@ -1,4 +1,4 @@
-System.register(["./application.b9bde.js"], function (_export, _context) {
+System.register(["./application.b32fe.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
