321c6a7f00fd16e26560b52ed62ca2fb2e9d287a
VaJoy Lan <vajoy@qq.com>
2025-11-24 02:21:20 +0800

    new

--- shader-demo/application.a4acc.js
+++ shader-demo/application.a4acc.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.2fc4f.json';
+          this.settingsPath = 'src/settings.a9e53.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
