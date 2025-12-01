ace14670674a7fa26b586a1ff024ffbd1f95c4e9
VaJoy Lan <vajoy@qq.com>
2025-12-01 17:46:14 +0800

    new

--- shader-demo/application.b584b.js
+++ shader-demo/application.b584b.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.34045.json';
+          this.settingsPath = 'src/settings.f710e.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- shader-demo/index.7e606.js
+++ shader-demo/index.7e606.js
@@ -1,4 +1,4 @@
-System.register(["./application.bde77.js"], function (_export, _context) {
+System.register(["./application.b584b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
