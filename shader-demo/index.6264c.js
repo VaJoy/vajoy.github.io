16359204041ff27492e194b0548aa9f3c1b54bf9
VaJoy Lan <vajoy@qq.com>
2025-12-01 16:07:02 +0800

    new

--- shader-demo/index.6264c.js
+++ shader-demo/index.6264c.js
@@ -1,4 +1,4 @@
-System.register(["./application.a4acc.js"], function (_export, _context) {
+System.register(["./application.207e6.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
