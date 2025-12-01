b01b93a9490a6654be8833be13c44f2688c5205c
VaJoy Lan <vajoy@qq.com>
2025-12-01 16:43:33 +0800

    new

--- shader-demo/index.1f321.js
+++ shader-demo/index.1f321.js
@@ -1,4 +1,4 @@
-System.register(["./application.207e6.js"], function (_export, _context) {
+System.register(["./application.bde77.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
